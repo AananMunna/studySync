15e835b1a11cbe282ee488ca82641e4e55174e3d
unknown <aananmunna420@gmail.com>
2025-06-06 00:41:48 +0600

    change hero logo for better UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
 import Lottie from "lottie-react";
-import studyAnimation from "../assets/study.json";
+import studyAnimation from "../assets/study2.json";
 
 export default function HeroSection() {
   return (
