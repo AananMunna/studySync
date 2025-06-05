585e202d98f4fd48999b2ec8ccfe106411db0e16
unknown <aananmunna420@gmail.com>
2025-06-05 12:47:22 +0600

    added a feature like when user change any route then the website will scroll to top or go that section where user want to go

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import FAQ from "./components/FAQ";
 import FeaturesSection from "./components/FeaturesSection";
 import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div>
+      <ScrollToTop />
       <Hero></Hero>
       <HowItWorks></HowItWorks>
       <FeaturesSection></FeaturesSection>
